702296eb283302eb4c942469292a2242ac613386
vinay kumar reddy <vinaykumarreddy@gmail.com>
2026-07-23 21:05:50 +0530

    updated prefix of routers

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
 )
 
 register_exception_handlers(app)
-app.include_router(api_router, prefix=settings.API_V1_PREFIX)
+app.include_router(api_router, prefix="/v1")
 
 
 @app.get("/health")
